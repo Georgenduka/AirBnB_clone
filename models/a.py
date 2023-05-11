9f5b48bdb7c529f0b51519d93786968dfdb552de
georgenduka <ndukageorgeveno@gmail.com>
2023-05-11 12:33:38 +0100

    base class that generates a dictionary representation of an instance

---
+++ models/a.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+create = 1
+
+def add_create():
+    create ++ 1
+
+print(add_create())
---
+++ models/base_model.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import cmd
+import uuid
+from datetime import datetime
+
+class BaseModel():
+
+    def __init__(self, *args, kwargs**:w):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        return ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dictionary = self.__dict__
+        dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        return dictionary
