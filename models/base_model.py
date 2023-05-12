21953fbec1ca8714f638d0b3bc3b4a777ac66f06
georgenduka <ndukageorgeveno@gmail.com>
2023-05-12 20:00:32 +0100

    variable number of arguments

--- models/base_model.py
+++ models/base_model.py
@@ -8,10 +8,10 @@ class BaseModel():
     def __init__(self, *args, **kwargs):
         if kwargs:
             for key, value in kwargs.items():
-                if key = __class__:
+                if key == __class__:
                     continue
-                elif key = "created_at" or key = "updated_at":
-                    format = %Y-%m-%dT%H:%M:%S.%f
+                elif key == "created_at" or key == "updated_at":
+                    format = '%Y %m %dT %H:%M:%S.%f'
                     self.__dict__[key] = datetime.strptime(self.__dict__[value], format)
                 else:
                     self.__dict__[key] = value
