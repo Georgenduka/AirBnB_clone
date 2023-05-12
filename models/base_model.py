c272e196e8ddae9f4078c3057cf3745048a643bb
georgenduka <ndukageorgeveno@gmail.com>
2023-05-12 20:00:32 +0100

    variable number of arguments

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,17 @@ from datetime import datetime
 
 class BaseModel():
 
-    def __init__(self, *args, kwargs**:w):
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key = __class__:
+                    continue
+                elif key = "created_at" or key = "updated_at":
+                    format = %Y-%m-%dT%H:%M:%S.%f
+                    self.__dict__[key] = datetime.strptime(self.__dict__[value], format)
+                else:
+                    self.__dict__[key] = value
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
