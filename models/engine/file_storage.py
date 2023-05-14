dead64c29d7d58cd8237be479e9d6c4f80fcaa7e
georgenduka <ndukageorgeveno@gmail.com>
2023-05-14 20:34:37 +0100

    pycodestyle fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,8 +2,10 @@
 import json
 from models.base_model import BaseModel
 
+
 class FileStorage:
-    """Class serializes instances to a JSON file and deserializes JSON file to instances"""
+    """Class serializes instances to a JSON file and
+    deserializes JSON file to instances"""
 
     __file_path = "file.json"
     __objects = {}
