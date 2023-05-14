21d171806759d8ebce761a0f77f90caaa752fb6b
georgenduka <ndukageorgeveno@gmail.com>
2023-05-14 19:57:17 +0100

    dont run when imported

--- console.py
+++ console.py
@@ -2,19 +2,20 @@
 import cmd
 
 
-class HBNBCommand(cmd.Cmd):
-    prompt = '(hbnb) '
+if __name__ == '__main__':
+    class HBNBCommand(cmd.Cmd):
+        prompt = '(hbnb) '
 
-    def emptyline(self):
-        pass
+        def emptyline(self):
+            pass
 
-    def do_quit(self, line):
-        """Quit command to exit the program"""
-        return True
+        def do_quit(self, line):
+            """Quit command to exit the program"""
+            return True
 
-    def do_EOF(self, line):
-        """Exit the program"""
-        return True
+        def do_EOF(self, line):
+            """Exit the program"""
+            return True
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
